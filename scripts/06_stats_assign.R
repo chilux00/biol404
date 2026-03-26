aa5bfa11614e5f5528b2f39c5a97b2a9a9c65c6b
chilux00 <chiingzhang@gmail.com>
2026-03-26 10:48:31 -0700

    add sim data for final project

---
+++ scripts/06_stats_assign.R
@@ -0,0 +1,114 @@
+# libraries
+library(tidyverse)
+library(vegan)
+
+# data
+data("dune")
+data("dune.env")
+species.data <- dune
+site.data <- dune.env
+
+species.dissim.mat <- vegdist(species.data, 
+                              method = "bray")
+
+# q2
+  ?vegdist
+  site.dissim.mat <- vegdist(site.data$A1, 
+                             method = "manhattan")
+  
+# q3
+  mantel(species.dissim.mat, 
+         site.dissim.mat, 
+         method = "pearson", 
+         permutations=9999)
+  cluster_fit <- hclust(species.dissim.mat, 
+                        method = "average")
+  plot(cluster_fit)  
+
+# q4
+  rect.hclust(cluster_fit, 
+              k = 4, 
+              border = "red")
+  rect.hclust(cluster_fit, 
+              h = 0.5, 
+              border = "red")  
+
+  env_cluster_fit <- hclust(site.dissim.mat, 
+                            method = "average")
+  plot(env_cluster_fit)
+  rect.hclust(env_cluster_fit, 
+              k = 3, 
+              border = "purple")
+
+# q5
+  species.NMDS <- metaMDS(species.data, 
+                          k=2)
+  species.NMDS  
+  plot(species.NMDS)  
+  ordiplot(species.NMDS, 
+           type = "none")  
+  orditorp(species.NMDS, 
+           display = "species", 
+           col = "red", 
+           air = 0.01)
+  orditorp(species.NMDS, 
+           display = "sites", 
+           cex = 0.75, 
+           air = 0.01)  
+  ordiplot(species.NMDS, 
+           type = "n")
+  ordihull(species.NMDS, 
+           groups = site.data$Use, 
+           draw = "polygon", 
+           col = "grey90", 
+           label = FALSE)
+  orditorp(species.NMDS, 
+           display = "species", 
+           col = "red", 
+           air = 0.01)
+  orditorp(species.NMDS, 
+           display = "sites", 
+           cex = 0.75, 
+           air = 0.01)  
+
+  ordiplot(species.NMDS, 
+           type = "none")  
+  ordispider(species.NMDS, 
+             groups = site.data$Use, 
+             col = "black", 
+             label = FALSE)
+  orditorp(species.NMDS, 
+           display = "species", 
+           col = "red", 
+           air = 0.01)
+  orditorp(species.NMDS, 
+           display = "sites", 
+           cex = 0.75, 
+           air = 0.01)  
+
+# q6
+  data(BCI)
+  data("BCI.env")
+  species.BCI <- BCI
+  environment.BCI <- BCI.env
+
+  BCI.species.dissim.mat <- vegdist(species.BCI, 
+                                    method = "euclidian")  
+  BCI.species.NMDS <- metaMDS(species.BCI, k = 2)
+  
+  ordiplot(BCI.species.NMDS, type = "n")
+  
+  ordihull(BCI.species.NMDS, 
+           groups = environment.BCI$Habitat, 
+           draw = "polygon", 
+           col = "grey90", 
+           label = FALSE)  
+
+  orditorp(BCI.species.NMDS, 
+           display = "sites", 
+           cex = 0.75, 
+           air = 0.01)
+  
+# q7
+  
+  
---
+++ scripts/data_cleaning_and_analysis.R
@@ -0,0 +1,85 @@
+#### Cleaning data + Running Analysis
+
+## Load libraries
+library(tidyverse)
+library(broom)
+library(dplyr)
+
+## Read in data
+data <- read_csv("ubc_herbivory_simulated_dataset.csv")
+
+## Mutate herbivory to be numerical
+
+# Define mapping
+herb_map <- c(
+  "0-10%" = 1,
+  "10-20%" = 2,
+  "20-30%" = 3,
+  "30-40%" = 4,
+  "40-50%" = 5,
+  "50-60%" = 6,
+  "60-70%" = 7,
+  "70-80%" = 8,
+  "80-90%" = 9,
+  "90-100%" = 10)
+
+
+data <- data %>%
+  mutate(
+    herbivory_low = herb_map[herbivory_low],
+    herbivory_med = herb_map[herbivory_med],
+    herbivory_high = herb_map[herbivory_high])
+
+## Structure data
+
+data <- data %>%
+  mutate(
+    type = factor(type),
+    sampler = factor(sampler),
+    herbivory_mean = (herbivory_low +
+                        herbivory_med +
+                        herbivory_high) / 3)
+
+## Compare groups to see mean herbivory
+
+mean_herbivory_table <- data %>%
+  group_by(type) %>%
+  summarise(
+    low_mean  = mean(herbivory_low),
+    med_mean  = mean(herbivory_med),
+    high_mean = mean(herbivory_high),
+    mean_all  = mean(herbivory_mean)
+  )
+
+mean_herbivory_table
+
+## Run linear models
+
+levels(data$type) ## Will tell us how to interpert the coefficients
+
+mod_low  <- lm(herbivory_low  ~ type + height + sampler, data = data)
+summary(mod_low)
+
+mod_med  <- lm(herbivory_med  ~ type + height + sampler, data = data)
+summary(mod_med)
+
+mod_high <- lm(herbivory_high ~ type + height + sampler, data = data)
+summary(mod_high)
+
+mod_mean <- lm(herbivory_mean ~ type + height + sampler, data = data)
+summary(mod_mean)
+
+## Make a table with results
+
+type_results <- bind_rows(
+  tidy(mod_low)  %>% filter(term == "typeNative") %>% mutate(model = "Low"),
+  tidy(mod_med)  %>% filter(term == "typeNative") %>% mutate(model = "Medium"),
+  tidy(mod_high) %>% filter(term == "typeNative") %>% mutate(model = "High"),
+  tidy(mod_mean) %>% filter(term == "typeNative") %>% mutate(model = "Mean")) %>%
+  select(model, estimate, std.error, p.value)
+
+type_results
+
+## Save table
+
+write_csv(type_results, "results/model_results")
